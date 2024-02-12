0f972f40c46f88b3384534beda095dcae41a31d2
“ITZTHEBEAST” <fredrik.inpitak@gmail.com>
2024-02-12 12:51:27 +0100

    Initial commit

---
+++ src/test/java/com/example/taskmana/TaskmanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
