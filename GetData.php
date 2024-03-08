b979c65d2f5c4606537a2855321fe4d4496d99c5
ITZTHEBEAST <Fredrik.inpitak@gmail.com>
2024-03-08 14:21:58 +0000

    lagt til kommentarer i GetData.php

--- GetData.php
+++ GetData.php
@@ -1,22 +1,24 @@
 <?php
+// Angi feilrapporteringsnivå for visning av feilmeldinger
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Database connection details
+// Detaljer for databaseforbindelse
 $servername = "172.20.128.69";
 $username = "fredrik";
 $password = "Skole123";
 $dbname = "MELDINGER";
 
+// Opprett en ny mysqli-forbindelse
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Sjekk tilkoblingen
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Fetch tasks from the database
+// Hent oppgaver fra databasen
 $sql = "SELECT * FROM tasks";
 $result = $conn->query($sql);
 
@@ -26,15 +28,17 @@ if ($result) {
         $tasks[] = array('taskId' => $row['id'], 'taskText' => $row['taskText']);
     }
 
+    // Sett opp svar i tilfelle suksess
     $response = array('status' => 'success', 'tasks' => $tasks);
 } else {
+    // Sett opp svar i tilfelle feil
     $response = array('status' => 'error', 'message' => 'Error fetching tasks: ' . $conn->error);
 }
 
-// Output response as JSON
+// Utgang respons som JSON
 header('Content-Type: application/json');
 echo json_encode($response);
 
-// Close the database connection
+// Lukk databaseforbindelsen
 $conn->close();
 ?>
